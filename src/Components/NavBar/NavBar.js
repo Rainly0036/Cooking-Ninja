7d3c87dd3e0e21e5b3d88785752a84e39985da7e
Rainly0036 <icetornado36@gmail.com>
2021-11-30 22:09:33 +0800

    .

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -5,10 +5,10 @@ import './NavBar.css'
 import { useTheme } from '../../Hooks/useTheme'
 
 export default function NavBar() {
-    const { color } = useTheme()
+    const { color, changeColor } = useTheme()
     return (
         <div className="navbar" style={{ background: color }}>
-            <nav>
+            <nav onClick={() => changeColor('#20bf50')}>
                 <Link to="/" className="brand">
                     <h1>Cooking Ninja</h1>
                 </Link>
--- src/Context/ThemeContext.js
+++ src/Context/ThemeContext.js
@@ -1,27 +1,28 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer } from 'react'
+
 export const ThemeContext = createContext()
 
 const themeReducer = (state, action) => {
-    switch (action.type) {
-        case 'CHANGE_COLOR':
-            return { ...state, color: action.payload }
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case 'CHANGE_COLOR':
+      return { ...state, color: action.payload }
+    default:
+      return state
+  }
 }
+
 export function ThemeProvider({ children }) {
+  const [state, dispatch] = useReducer(themeReducer, {
+    color: '#58249c'
+  })
 
-    const [state, dispatch] = useReducer(themeReducer, {
-        color: '#20bf50',
-    })
+  const changeColor = (color) => {
+    dispatch({ type: 'CHANGE_COLOR', payload: color })
+  }
 
-    const changeColor = (color) => {
-        dispatch({ type: 'CHANGE_COLOR', color })
-    }
-
-    return (
-        <ThemeContext.Provider value={{ ...state, changeColor }}>
-            {children}
-        </ThemeContext.Provider>
-    )
+  return (
+    <ThemeContext.Provider value={{...state, changeColor}}>
+      {children}
+    </ThemeContext.Provider>
+  )
 }

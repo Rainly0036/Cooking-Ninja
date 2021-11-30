c11523250c8210d20c4fb4516ca0cdcf3b40955e
Rainly0036 <icetornado36@gmail.com>
2021-11-30 21:01:41 +0800

    .

--- src/Context/ThemeContext.js
+++ src/Context/ThemeContext.js
@@ -1,9 +1,26 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 export const ThemeContext = createContext()
 
+const themeReducer = (state, action) => {
+    switch (action.type) {
+        case 'CHANGE_COLOR':
+            return { ...state, color: action.payload }
+        default:
+            return state;
+    }
+}
 export function ThemeProvider({ children }) {
+
+    const [state, dispatch] = useReducer(themeReducer, {
+        color: '#20bf50',
+    })
+
+    const changeColor = (color) => {
+        dispatch({ type: 'CHANGE_COLOR', color })
+    }
+
     return (
-        <ThemeContext.Provider value={{ color: '#20bf50' }}>
+        <ThemeContext.Provider value={{ ...state, changeColor }}>
             {children}
         </ThemeContext.Provider>
     )
